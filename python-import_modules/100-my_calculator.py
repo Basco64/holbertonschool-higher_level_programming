98a7faa1ff51eae6899b2e84f1149455fe878570
Basco <martinez.xabi@laposte.net>
2024-09-04 09:26:24 +0000

    Fix task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,21 +4,21 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(argv) != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
 
-    if op is '+':
+    if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op is '-':
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op is '*':
+    elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif op is '/':
+    elif op == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
