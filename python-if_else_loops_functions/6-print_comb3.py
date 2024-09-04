5f2219e1ea1e255781645ae7945f40791b4af282
Basco <martinez.xabi@laposte.net>
2024-09-04 08:05:02 +0000

    Fix task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for firstNum in range(9):
-        for secondNum in range(1, 10):
+for firstNum in range(0, 9):
+    for secondNum in range(1, 10):
             if secondNum > firstNum:
                 print("{}{}".format(firstNum, secondNum), end="")
                 if not (firstNum == 8 and secondNum == 9):
