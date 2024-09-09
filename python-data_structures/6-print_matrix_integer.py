dd24318ee91a006dce536e455573506c256cb6e9
Basco <martinez.xabi@laposte.net>
2024-09-09 08:37:46 +0000

    Project 0

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for line in matrix:
+        for num in line:
+            print("{}".format(num), end=" " if num != line[-1] else "")
+        print()
