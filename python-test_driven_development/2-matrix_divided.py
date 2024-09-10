e137af131f9818bc12040689519374899bfaf0cb
Basco <martinez.xabi@laposte.net>
2024-09-10 13:39:20 +0000

    Fix pycodestyle error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -32,16 +32,16 @@ def matrix_divided(matrix, div):
     line_len = None
 
     for line in matrix:
-         if not isinstance(line, list) or len(line) == 0:
-             raise TypeError(error_msg)
-         
-         if line_len is None:
-             line_len = len(line)
-         elif len(line) != line_len:
-             raise TypeError("Each row of the matrix must have the same size")
+        if not isinstance(line, list) or len(line) == 0:
+            raise TypeError(error_msg)
 
-         for num in line:
-             if not isinstance(num, int) and not isinstance(num, float):
-                 raise TypeError(error_msg)
+        if line_len is None:
+            line_len = len(line)
+        elif len(line) != line_len:
+            raise TypeError("Each row of the matrix must have the same size")
 
-    return [[round(num /div, 2) for num in line] for line in matrix]
+        for num in line:
+            if not isinstance(num, int) and not isinstance(num, float):
+                raise TypeError(error_msg)
+
+    return [[round(num / div, 2) for num in line] for line in matrix]
