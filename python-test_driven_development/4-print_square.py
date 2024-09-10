085b19c25cd15123c99495602a7c23a2de13279b
Basco <martinez.xabi@laposte.net>
2024-09-10 14:03:06 +0000

    Resolve task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defines print_square function."""
+
+def print_square(size):
+
+    """
+    Prints a square with '#'.
+
+    size: The size of the square.
+
+    Return: Nothing
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+         print("#" * size)
