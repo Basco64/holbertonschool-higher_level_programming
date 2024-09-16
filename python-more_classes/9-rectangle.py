8b1932c2cd58513d52fa454f987a418d6fdd1d10
Basco <martinez.xabi@laposte.net>
2024-09-16 16:48:43 +0000

    The angry checker again

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -82,4 +82,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Creates an instance where Rectangle is a square"""
-        return cls(size, size)
+        return Rectangle(size, size)
