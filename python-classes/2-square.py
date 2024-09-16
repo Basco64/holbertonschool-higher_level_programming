5bceae1fd53f25564fd7a6de14b602e57bf4b638
Basco <martinez.xabi@laposte.net>
2024-09-16 10:39:46 +0000

    Add Task 2 documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,7 +3,10 @@
 
 
 class Square:
+    """Define a square"""
+
     def __init__(self, size=0):
+        """Initialize the square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
