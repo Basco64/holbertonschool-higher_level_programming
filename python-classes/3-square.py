d78d0c8e28dddf2fb29230b2a3f13078ee398c32
Basco <martinez.xabi@laposte.net>
2024-09-16 10:42:29 +0000

    Add Task 3 documentation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,7 +3,10 @@
 
 
 class Square:
+    """Define a square"""
+
     def __init__(self, size=0):
+        """Initialize the square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -11,4 +14,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Return the area of the square"""
         return self.__size ** 2
