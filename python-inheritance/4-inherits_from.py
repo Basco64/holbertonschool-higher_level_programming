735a788a9912b2ab1b29b3e5a5da4adfce63db66
Basco <martinez.xabi@laposte.net>
2024-09-23 09:38:26 +0000

    Fix pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@
 def inherits_from(obj, a_class):
     """inherits from"""
 
-    if (!isinstance(obj, a_class):
+    if isinstance(obj, a_class):
         return False
     return issubclass(type(obj), a_class)
