df08b27470786ea200f69b6a1248a3e71d56662b
Basco <martinez.xabi@laposte.net>
2024-09-23 11:02:08 +0000

    Resolve task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+
+class BaseGeometry:
+    """base geometry"""
+
+    def area(self):
+        """exception if area is not implemented"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """is an integer?"""
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+    def __init__(self, width, height):
+        """initialization method"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
