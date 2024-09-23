4b8097a4689cd7c4e19c4aebbdcd4ac2c65f2dbb
Basco <martinez.xabi@laposte.net>
2024-09-23 08:25:46 +0000

    Resolve task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""check classes"""
+
+def is_same_class(obj, a_class):
+    """check classes"""
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
