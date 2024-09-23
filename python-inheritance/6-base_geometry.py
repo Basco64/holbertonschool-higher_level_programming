60e3076d34e69961ef8f1cde3bcdab33f63ef18f
Basco <martinez.xabi@laposte.net>
2024-09-23 08:37:47 +0000

    Resolve task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""base geometry"""
+
+class BaseGeometry:
+    """base geometry"""
+
+    def area(self):
+        """exception if area is not implemented"""
+        raise Exception('area() is not implemented')
