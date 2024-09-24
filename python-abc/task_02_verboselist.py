832527937f620476970578159c585569d255da55
Basco <martinez.xabi@laposte.net>
2024-09-24 06:37:41 +0000

    Resolve task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from abc import ABC, abstractmethod
+
+class VerboseList(list):
+    """verboselist class"""
+
+    def append(self, item):
+        """append method"""
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        """extend method"""
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
+
+    def remove(self, item):
+        """remove item"""
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        """pop method"""
+        item = super().pop(index)
+        print("Popped [{}] from the list.".format(item))
+        return item
+
+
