588bb6838dfa656811ea62b369957bb497bea2c8
Basco <martinez.xabi@laposte.net>
2024-09-24 05:46:13 +0000

    Fix task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """initialization method"""
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
