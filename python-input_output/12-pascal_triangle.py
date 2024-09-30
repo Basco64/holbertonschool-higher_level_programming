ab7f19c00f794dfae81486e7e6ccbbccb1d2b81d
Basco <martinez.xabi@laposte.net>
2024-09-30 11:32:51 +0000

    Resolve task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            for j in range(1, len(last_row)):
+                row.append(last_row[j - 1] + last_row[j])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
