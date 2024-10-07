32c9c9cc30038d30f0ff5489eec6ceead2dea226
Basco <martinez.xabi@laposte.net>
2024-10-07 08:03:09 +0000

    Resolve task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+url = "https://jsonplaceholder.typicode.com/posts"
+req = requests.get(url)
+
+def fetch_and_print_posts():
+    print(f"Status Code: {req.status_code}")
+    
+    if req.status_code == 200:
+        posts = req.json()
+
+        for post in posts:
+            print(f"{post['title']}")
+    else:
+        print("Error print")
+
+
+def fetch_and_save_posts():
+    if req.status_code == 200:
+        data = []
+
+        posts = req.json()
+        for post in posts:
+            obj = {"id" : post['id'],
+                    "title" : post['title'],
+                    "body" : post['body']
+                    }
+            data.append(obj)
+
+        csv_col = ['id', 'title', 'body']
+
+        with open('posts.csv', mode='w') as file:
+            writer = csv.DictWriter(file, fieldnames=csv_col)
+            writer.writeheader()
+            writer.writerows(data)
+
+    else:
+        print("Error save")
+
