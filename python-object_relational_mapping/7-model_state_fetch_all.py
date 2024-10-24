17f5c04257be0bea15d85a6eb8a19d595807a015
Basco <martinez.xabi@laposte.net>
2024-10-24 19:01:06 +0200

    ORM

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,19 +12,6 @@ class State(Base):
     """State class"""
 
     __tablename__ = 'states'
-
+    
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-engine = create_engine(
-  'mysql+mysqldb://username/:password@localhost:3306/db_name',
-  pool_pre_ping=True)
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-new_state = State(name='California')
-session.add(new_state)
-session.commit()
