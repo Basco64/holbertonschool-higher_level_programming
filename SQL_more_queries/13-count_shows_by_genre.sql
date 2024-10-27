a4c61dd86d5bc60f9dece4634f3cc3bdfa02e5c8
Basco <martinez.xabi@laposte.net>
2024-10-27 22:18:38 +0100

    more queries

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- 13-count_shows_by_genre.sql
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-GROUP BY tv_show_genres.genre_id
-HAVING COUNT(tv_shows.id) > 0
+SELECT tv_genres.name genre,
+COUNT(*) number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,10 @@
 -- 14-my_genres.sql
 SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = `Dexter`
-ORDER BY tv_genres.name ASC;
+  FROM tv_genres
+       INNER JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+
+       INNER JOIN tv_shows
+       ON tv_shows.id = tv_show_genres.show_id
+       WHERE tv_shows.title = "Dexter"
+ ORDER BY tv_genres.name;
