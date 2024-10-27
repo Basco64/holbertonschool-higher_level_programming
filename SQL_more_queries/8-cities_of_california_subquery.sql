18e7e1cb8174471fc8fe65d2989145ab8f9497c5
Basco <martinez.xabi@laposte.net>
2024-10-27 21:47:51 +0100

    more queries

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- 8-cities_of_california_subquery.sql
-USE `hbtn_0d_usa`;
-SELECT cities.id, cities.name 
-FROM cities, states 
+SELECT cities.id, cities.name
+FROM cities, states
 WHERE cities.state_id = states.id AND states.name = `California`
 ORDER BY cities.id ASC;
