3b55aae62fec4adba15d5e90d22bc2ee4121813c
Basco <martinez.xabi@laposte.net>
2024-10-27 21:54:29 +0100

    more queries

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- 13-count_shows_by_genre.sql
-USE `hbtn_0d_tvshows`;
 SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_show_genres
 LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,4 @@
 -- 14-my_genres.sql
-USE `hbtn_0d_tvshows`;
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
