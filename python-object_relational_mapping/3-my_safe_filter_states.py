576acfe44dc24a2940963fae7f22b56e5a282f3f
Basco <martinez.xabi@laposte.net>
2024-10-28 15:06:57 +0100

    ORM

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,36 +5,28 @@ import sys
 import MySQLdb
 
 
-def filter_states(username, password, database, state_name):
-    """ Filter states by user input safely """
+if __name__ == "__main__":
+    # Connect to MySQL database
     db = MySQLdb.connect(
-      host="localhost",
-      port=3306,
-      user=username,
-      pwd=password,
-      db=database
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-    query = (
-        "SELECT * FROM states "
-        "WHERE name = %s "
-        "ORDER BY id ASC"
-    )
+
+    # Create cursor object
     cursor = db.cursor()
-    cursor.execute(query, (state_name))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+
+    # Create and execute query with parameterized query for safety
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (sys.argv[4],))
+
+    # Fetch and display results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close database connection
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    """ Checking arguments and execution"""
-    if len(sys.argv) != 4:
-        print("Usage: ./3-my_safe_filter_states.py "
-              "<mysql username> "
-              "<mysql password> "
-              "<database name> "
-              "<state name>")
-    else:
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
