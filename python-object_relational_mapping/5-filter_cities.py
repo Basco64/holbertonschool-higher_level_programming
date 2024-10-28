990afeff1b9d0de74c47ca83f3e63e987a468674
Basco <martinez.xabi@laposte.net>
2024-10-28 15:27:32 +0100

    ORM

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Create and execute query
-     query = "SELECT cities.name \
+    query = "SELECT cities.name \
               FROM cities \
               JOIN states ON cities.state_id = states.id \
               WHERE states.name = %s \
               ORDER BY cities.id ASC"
-    cursor.execute(query, (sys.argv[4]))
+    cursor.execute(query, sys.argv[4])
 
     # Fetch and display results
     cities = cursor.fetchall()
