8c8e0c430168ae7370ca850e602949f8f6f81dc5
Basco <martinez.xabi@laposte.net>
2024-10-28 15:31:20 +0100

    ORM

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,16 +19,18 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Create and execute query
-    query = "SELECT cities.name \
-              FROM cities \
-              JOIN states ON cities.state_id = states.id \
-              WHERE states.name = %s \
-              ORDER BY cities.id ASC"
-    cursor.execute(query, sys.argv[4])
+    query = """SELECT cities.name 
+              FROM cities 
+              JOIN states ON cities.state_id = states.id 
+              WHERE states.name = %s 
+              ORDER BY cities.id ASC"""
+    cursor.execute(query, (sys.argv[4],))
 
-    # Fetch and display results
+    # Fetch results
     cities = cursor.fetchall()
-    print(", ".join(city[0] for city in cities))
+
+    # Print results
+     print(", ".join(city[0] for city in cities))
 
     # Close database connection
     cursor.close()
