690a66bca4d966ec29b9112da1dea09aa55f719d
Basco <martinez.xabi@laposte.net>
2024-10-28 09:49:17 +0100

    ORM

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,7 @@
 """ 0-select_states """
 
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-Base = declarative_base()
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
